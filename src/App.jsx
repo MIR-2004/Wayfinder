8844470df83f41aab17713b72e95dda2955919e7
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2024-12-01 15:37:24 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Button} from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h2>welcome</h2>
+      <Button>click me</Button>
+    </>
+  )
+}
+
+export default App
